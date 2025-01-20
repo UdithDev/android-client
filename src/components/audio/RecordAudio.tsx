8ff02d49b84e9c33b07a557429c576f117fddb8d
daveudith@gmail.com <daveudith@gmail.com>
2025-01-20 11:09:23 +0530

    initialize new component RecordAudio.tsx

---
+++ src/components/audio/RecordAudio.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, View, Text } from "react-native";
+
+const RecordAudio: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <View>
+          <Text>This is a RecordAudio screen</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default RecordAudio;
--- src/components/audio/index.ts
+++ src/components/audio/index.ts
@@ -1,2 +1,3 @@
 export { default as AudioToolsButtons } from "./AudioToolsButtons";
 export { default as ImportAudio } from "./ImportAudio";
+export { default as RecordAudio } from "./RecordAudio";
